b0309a77c979481138aa0204e968fb6fe5ec341f
Isaac <engrisaac1234@gmail.com>
2022-01-16 21:35:11 +0100

    fork it with youtube link

---
+++ anonapp/templatetags/youtube.py
@@ -0,0 +1,14 @@
+from django import template
+import re
+
+
+register = template.Library()
+
+
+def format_youtube(value):
+    yt_link = re.compile(r'(https?://)?(www\.)?((youtu\.be/)|(youtube\.com/watch/?\?v=))([A-Za-z0-9-_]+)', re.I)
+    yt_embed = '<iframe width="560" height="315" src="https://www.youtube.com/embed/{0}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    return yt_link.sub(lambda match: yt_embed.format(match.groups()[5]), value)
+
+
+register.filter('format_youtube',format_youtube)
