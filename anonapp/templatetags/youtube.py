c2ce4f734c65e2814ac03e305ca7bb03e3efd265
Isaac <engrisaac1234@gmail.com>
2022-01-22 16:33:53 +0100

    cc

--- anonapp/templatetags/youtube.py
+++ anonapp/templatetags/youtube.py
@@ -1,5 +1,4 @@
 from django import template
-from django.utils.html import mark_safe, escape
 import re
 
 
@@ -8,8 +7,9 @@ register = template.Library()
 
 def format_youtube(value):
     yt_link = re.compile(r'(https?://)?(www\.)?((youtu\.be/)|(youtube\.com/watch/?\?v=))([A-Za-z0-9-_]+)', re.I)
-    yt_embed = '<iframe width="560" height="315" src="https://www.youtube.com/embed/{0}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    yt_embed = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/{0}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
     return yt_link.sub(lambda match: yt_embed.format(match.groups()[5]), value)
+    
 
 
 register.filter('format_youtube',format_youtube)
