1a1f5187e71e9c7c1982e3eb7fd00091e872df90
Isaac <engrisaac1234@gmail.com>
2022-01-25 03:13:29 +0100

    aas

--- anonproject/settings.py
+++ anonproject/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.sitemaps',
-    'django.contrib.sites',
+    # 'django.contrib.sites',
     'django.contrib.staticfiles',
 ]
 
